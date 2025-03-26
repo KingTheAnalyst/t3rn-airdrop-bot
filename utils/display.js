4b2d96763662b8b5efb13a86206be1356b975e82
Kingkhuta <157789487+KingTheAnalyst@users.noreply.github.com>
2025-03-26 07:54:06 +0100

    Update display.js

--- utils/display.js
+++ utils/display.js
@@ -4,8 +4,8 @@ function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
   console.log('=      🚀 T3rn Auto Bridge Bot 🚀      ='.cyan);
-  console.log('=     Created by HappyCuanAirdrop      ='.cyan);
-  console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
+  console.log('=     Modified by kingkhuta      ='.cyan);
+  console.log('=    original made by https://t.me/HappyCuanAirdrop     ='.cyan);
   console.log('========================================'.cyan);
   console.log();
 }
